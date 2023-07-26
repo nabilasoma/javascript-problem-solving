2ca72cbd0a39de173de9a443c5f594a44f273c77
unknown <nabilasoma@gmail.com>
2023-07-26 19:59:36 +0600

    added function of second largest number

--- index.js
+++ index.js
@@ -142,3 +142,33 @@ const generateRandomPassword = length => {
 const password = generateRandomPassword(10);
 console.log(password);
 
+
+
+
+// Task 8: Implement a JavaScript function to find the second smallest element in an array of numbers. The function should return the second smallest number.
+
+const findSecondSmallest = arr => {
+    if(arr.length < 2){
+        return "Array should have at least two elements";
+    }
+
+
+    let smallest = Infinity;
+    let secondSmallest = Infinity;
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] < smallest){
+            secondSmallest = smallest;
+            smallest = arr[i];
+        }else if(arr[i] < secondSmallest && arr[i] !== smallest){
+            secondSmallest = arr[i];
+        }
+    }
+    return secondSmallest;
+}
+
+
+console.log(findSecondSmallest([5, 2, 8, 3, 1, 9])); // Output: 2
+
+
+
